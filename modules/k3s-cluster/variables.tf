20061a5de5c63bb03cedb5d093107036c22ca497
iaminpwd <ampfamot@gmail.com>
2026-04-09 06:05:30 +0000

    .

--- modules/k3s-cluster/variables.tf
+++ modules/k3s-cluster/variables.tf
@@ -1,5 +1,4 @@
 variable "master_instance_type" { type = string }
-variable "worker_instance_type" { type = string }
 variable "sg_id" { type = string }
 variable "subnet_id" { type = string }
 variable "key_name" { type = string }
